b7e0d7ffe5acc48ad6a6b17349172b455886c1ec
CHANDRASHEKHAR_NATH_DUBEY <chandrashekhar.nath.dubey@oracle.com>
2021-08-09 22:52:41 +0530

    added queries to create database

---
+++ CREATE_DATABASE_AND_TABLES.sql
@@ -0,0 +1,54 @@
+CREATE DATABASE IF NOT EXISTS `E_COMMERCE`;
+CREATE TABLE IF NOT EXISTS `E_COMMERCE`.`USER_DETAILS` (
+		user_id bigint not null auto_increment,
+        user_name varchar(255) default null,
+        password varchar(255) default null,
+        user_address json default null,
+        primary key (user_id));
+CREATE TABLE IF NOT EXISTS `E_COMMERCE`.`PRODUCT_TYPE_DETAILS` (
+		product_type_id bigint not null auto_increment,
+        product_type_name varchar(255) default null,
+        gender enum("MALE", "FEMALE", "UNISEX"), 
+        primary key (product_type_id));
+CREATE TABLE IF NOT EXISTS `E_COMMERCE`.`PRODUCT` (
+		product_id bigint not null auto_increment,
+        product_name varchar(255) default null,
+        cost double default 0.0,
+        quantity integer default 0,
+        available_delivery_list json default null,
+        discount integer default 0, 
+        primary key (product_id));
+CREATE TABLE IF NOT EXISTS `E_COMMERCE`.`PRODUCT_CATEGORY` (
+		product_id bigint not null,
+        product_type_id bigint not null,
+        product_category enum("<10000", "<100000", ">100000"),
+        foreign key (product_id) references PRODUCT(product_id),
+        foreign key (product_type_id) references PRODUCT_TYPE_DETAILS(product_type_id));
+CREATE TABLE IF NOT EXISTS `E_COMMERCE`.`CART` (
+		product_id bigint not null,
+        user_id bigint not null,
+        foreign key (product_id) references PRODUCT(product_id),
+        foreign key (user_id) references USER_DETAILS(user_id));
+CREATE TABLE IF NOT EXISTS `E_COMMERCE`.`WISHLIST` (
+		product_id bigint not null,
+        user_id bigint not null,
+        foreign key (product_id) references PRODUCT(product_id),
+        foreign key (user_id) references USER_DETAILS(user_id));
+CREATE TABLE IF NOT EXISTS `E_COMMERCE`.`ORDER` (
+		order_id bigint not null auto_increment,
+		product_id bigint not null,
+        user_id bigint not null,
+        last_tracked_location varchar(255) default null,
+        primary key (order_id),
+        foreign key (product_id) references PRODUCT(product_id),
+        foreign key (user_id) references USER_DETAILS(user_id));
+CREATE TABLE IF NOT EXISTS `E_COMMERCE`.`TRENDING_PRODUCTS` (
+		product_id bigint not null,
+        no_of_times_product_is_sold integer default 0,
+        foreign key (product_id) references PRODUCT(product_id));
+CREATE TABLE IF NOT EXISTS `E_COMMERCE`.`PAYMENT_DETAILS` (
+		payment_type varchar(255) default null,
+        card_upi_number decimal(16) not null,
+        user_id bigint not null,
+        primary key(card_upi_number),
+        foreign key (user_id) references USER_DETAILS(user_id));
